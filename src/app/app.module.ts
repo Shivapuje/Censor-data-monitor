f7c01aa793c44f9f5c255ffba52d7c6606578887
Vinayak Shivapuje <vinayak.shivapuje@outlook.com>
2018-05-23 16:57:24 +0530

    First version of the dashboard, with basic values displaying functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { DataService } from './data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WeatherReportComponent } from './weather-report/weather-report.component';
+import { DayNightSensorComponent } from './day-night-sensor/day-night-sensor.component';
+import { BusStopMonitorComponent } from './bus-stop-monitor/bus-stop-monitor.component';
+import { VehicleMonitorComponent } from './vehicle-monitor/vehicle-monitor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    WeatherReportComponent,
+    DayNightSensorComponent,
+    BusStopMonitorComponent,
+    VehicleMonitorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
